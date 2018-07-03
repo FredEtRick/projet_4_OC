a7426fb9a55819d97e338d1003907577dfaf7718
travail <travail@MacBook-Pro-de-frederic.local>
2018-07-03 15:45:47 +0200

    testGetTarifJourneeMoins4 modifié et achevé, date du jour - 3 ans

--- tests/PersonneBundle/Entity/PersonneTest.php
+++ tests/PersonneBundle/Entity/PersonneTest.php
@@ -9,31 +9,13 @@
         public function testGetTarifJourneeMoins4()
         {
             $bebe = new Personne();
-            $aujourdhui = date('d-m-Y');
-            $jour = substr($aujourdhui, 0, 2);
-            $mois = substr($aujourdhui, 3, 2);
-            $annee = substr($aujourdhui, 6);
-            $annee -= 4;
-            if ($jour > 1)
-                $jour--;
-            // TROMPE !!! IL FAUT AJOUTER UN JOUR PAS LE RETIRER.
-            elseif ($jour == 1 && mois > 1)
-            {
-                $m30 = array(4, 6, 9, 11);
-                $m31 = array(3, 5, 7, 8, 10, 12);
-                if (($mois - 1) == 2)
-                {
-                    
-                }
-                if (in_array(($mois - 1), $m30))
-                    $jour = 30;
-                if (in_array(($mois - 1), $m31))
-                    $jour = 31;
-                $mois--;
-            }
+            $naissance = date('d-m-Y');
+            $annee = substr($naissance, 6);
+            $annee -= 3;
             $naissance = substr($naissance, 0, 6) . $annee;
-            echo 'NAISSANCE : ' . $naissance;
-            $bebe->setDateNaissance(new \DateTime('01-10-2016'));
+            echo 'naissance : ' . $naissance;
+            $naissance = date_create_from_format('d-m-Y', $naissance);
+            $bebe->setDateNaissance($naissance);
             $bebe->setReduction(false);
             
             $result = $bebe->getTarifJournee();
