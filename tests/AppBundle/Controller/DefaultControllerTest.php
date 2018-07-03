4cd6456354e060492e218a534aa2cef4396e3248
travail <travail@MacBook-Pro-de-frederic.local>
2018-07-03 15:11:54 +0200

    bugs corrigés avec johan hier, reprend PersonneTest aujourdhui.

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -11,6 +11,11 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+        /*echo $crawler->html();
+        
+        $response = $client->getResponse();
+        
+        if (!$response->isSuccessful()) {     $block = $crawler->filter('div.text-exception > h1');     if ($block->count()) {         $error = $block->text();     } }*/
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
--- tests/PersonneBundle/Entity/PersonneTest.php
+++ tests/PersonneBundle/Entity/PersonneTest.php
@@ -8,12 +8,42 @@
     {
         public function testGetTarifJourneeMoins4()
         {
-            $enfant = new Personne();
-            $enfant->setDateNaissance(new \DateTime('01-10-2016'));
-            $enfant->setReduction(false);
+            $bebe = new Personne();
+            $aujourdhui = date('d-m-Y');
+            $jour = substr($aujourdhui, 0, 2);
+            $mois = substr($aujourdhui, 3, 2);
+            $annee = substr($aujourdhui, 6);
+            $annee -= 4;
+            if ($jour > 1)
+                $jour--;
+            // TROMPE !!! IL FAUT AJOUTER UN JOUR PAS LE RETIRER.
+            elseif ($jour == 1 && mois > 1)
+            {
+                $m30 = array(4, 6, 9, 11);
+                $m31 = array(3, 5, 7, 8, 10, 12);
+                if (($mois - 1) == 2)
+                {
+                    
+                }
+                if (in_array(($mois - 1), $m30))
+                    $jour = 30;
+                if (in_array(($mois - 1), $m31))
+                    $jour = 31;
+                $mois--;
+            }
+            $naissance = substr($naissance, 0, 6) . $annee;
+            echo 'NAISSANCE : ' . $naissance;
+            $bebe->setDateNaissance(new \DateTime('01-10-2016'));
+            $bebe->setReduction(false);
             
-            $result = $enfant->getTarifJournee();
+            $result = $bebe->getTarifJournee();
             
             $this->assertSame(0, $result);
         }
+        
+        /*public function testGetTarifJourneeEnfant()
+        {
+            $enfant = new Personne();
+            $enfant->setDateNaissance();
+        }*/
     }
