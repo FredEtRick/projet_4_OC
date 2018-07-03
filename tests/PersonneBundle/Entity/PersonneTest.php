fad4935dd6fb3023889fe759a650b1b3a092af2e
travail <travail@MacBook-Pro-de-frederic.local>
2018-07-03 15:51:19 +0200

    test enfant de 4 a 12 ans ajouté et fonctionnel, exemple avec 6 ans

--- tests/PersonneBundle/Entity/PersonneTest.php
+++ tests/PersonneBundle/Entity/PersonneTest.php
@@ -11,21 +11,30 @@
             $bebe = new Personne();
             $naissance = date('d-m-Y');
             $annee = substr($naissance, 6);
-            $annee -= 3;
+            $annee -= 3; // Enfant de 3 ans en guise d'exemple, ne prend plus de date fixe en toutes lettres mais date du jour moins 3 ans, pour que le test soit bon même si on le refait dans plusieurs années.
             $naissance = substr($naissance, 0, 6) . $annee;
             echo 'naissance : ' . $naissance;
             $naissance = date_create_from_format('d-m-Y', $naissance);
             $bebe->setDateNaissance($naissance);
-            $bebe->setReduction(false);
             
             $result = $bebe->getTarifJournee();
             
             $this->assertSame(0, $result);
         }
         
-        /*public function testGetTarifJourneeEnfant()
+        public function testGetTarifJourneeEnfant() // 4 a 12 compris
         {
             $enfant = new Personne();
-            $enfant->setDateNaissance();
-        }*/
+            $naissance = date('d-m-Y');
+            $annee = substr($naissance, 6);
+            $annee -= 6;
+            $naissance = substr($naissance, 0, 6) . $annee;
+            echo 'naissance : ' . $naissance;
+            $naissance = date_create_from_format('d-m-Y', $naissance);
+            $enfant->setDateNaissance($naissance);
+            
+            $result = $enfant->getTarifJournee();
+            
+            $this->assertSame(8, $result);
+        }
     }
